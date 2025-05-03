563230d048bafb57b8d6b5ccadf0828b5bd5e98b
aneemoreeno1 <amorenoruiz146@gmail.com>
2025-05-03 12:36:02 +0200

    MapaEmpty-ko etsaiekin arazoak konponduta

--- BombermanProiektua/src/bmberman/src/packModeloa/MapaEmpty.java
+++ BombermanProiektua/src/bmberman/src/packModeloa/MapaEmpty.java
@@ -12,16 +12,25 @@ public class MapaEmpty extends BlokeMapa{
 	        for (int i = 0; i < filak; i++) {
 	            for (int j = 0; j < zutabeak; j++) {
 	                mapa[i][j] = new Kuadrikula();
-	                if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) continue;
-	                else if (random.nextDouble() * 100 > 50 && etsaikop < 10) {
-	                    Etsaia etsaia = new Etsaia(j, i);
-	                    etsaiak.add(etsaia);
-	                    mapa[i][j].setEtsaia(etsaia);
-	                    etsaikop++;
+	            }
+	        }  
+	        mapa[0][0].setBomberman(bmberman);
+	        etsaikop = 0;
+	        for (int i = 0; i < filak; i++) {
+	        	for (int j = 0; j < zutabeak; j++) {
+	                if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
+	                	continue;
+	                }
+	                
+	                double probabilitatea = random.nextDouble() * 100;	                
+	                if (probabilitatea > 95 && etsaikop <10) {
+	                	Etsaia etsaia = new Etsaia(j, i);
+	                	etsaiak.add(etsaia);
+	                	mapa[i][j].setEtsaia(etsaia);
+	                	etsaikop++;
 	                }
 	            }
 	        }
-	        mapa[0][0].setBomberman(bmberman);
 	        etsaienMugimendua();
 	    }
 	}
